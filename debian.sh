3c1ef3e4ca6171a8abdeaf244a790984349786b0
KaptanLingu <128973980+KaptanLingu@users.noreply.github.com>
2023-10-21 22:39:26 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,4 +2,4 @@
 
 apt install gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions gdm3 eog file-roller nemo mousepad vlc power-profiles-daemon xdg-user-dirs fish nala
 chsh -s /usr/bin/fish
-sudo reboot
+reboot
