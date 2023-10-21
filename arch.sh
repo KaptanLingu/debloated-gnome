7f0cd0d7cd23dc976f1ec23e97b691e3ee793cc4
KaptanLingu <128973980+KaptanLingu@users.noreply.github.com>
2023-10-21 22:38:59 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+sudo pacman -S gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions gdm eog file-roller nemo mousepad vlc power-profiles-daemon xdg-user-dirs fish 
+sudo systemctl enable gdm
+chsh -s /usr/bin/fish
+sudo reboot
