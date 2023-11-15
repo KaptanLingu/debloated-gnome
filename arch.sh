f18ba9dde27c60552422f11e129b38af551cb995
EMB <128973980+KaptanLingu@users.noreply.github.com>
2023-11-15 21:00:23 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo pacman -Syu
 sudo pacman -S gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions chrome-gnome-shell gdm eog file-roller nemo mousepad vlc power-profiles-daemon xdg-user-dirs fish 
 sudo systemctl enable gdm
 chsh -s /usr/bin/fish
