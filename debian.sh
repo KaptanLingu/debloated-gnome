d4987cd1e0970499a9e3cc6e6d8256a708fcd771
EMB <128973980+KaptanLingu@users.noreply.github.com>
2024-05-22 12:56:16 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update -qq
 sudo apt upgrade -yy
-sudo apt install -y nala
-sudo nala install -y gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions chrome-gnome-shell gdm3 eog file-roller nemo mousepad vlc power-profiles-daemon xdg-user-dirs fish sudo
-chsh -s /usr/bin/fish
+sudo apt install -y gnome-session gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions chrome-gnome-shell gdm3 eog file-roller nautilus mousepad vlc power-profiles-daemon xdg-user-dirs
+xdg-user-dirs-update
 sudo reboot
