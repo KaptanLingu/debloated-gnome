d102c7c00849efca26943befdce24f8f135809ef
EMB <128973980+KaptanLingu@users.noreply.github.com>
2024-05-22 12:56:20 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions chrome-gnome-shell gdm eog file-roller nemo mousepad vlc power-profiles-daemon xdg-user-dirs fish 
+sudo pacman -S gnome-shell gnome-control-center gnome-tweaks gnome-terminal gnome-software gnome-system-monitor gnome-shell-extensions chrome-gnome-shell gdm eog file-roller nautilus mousepad vlc power-profiles-daemon xdg-user-dirs 
 sudo systemctl enable gdm
-chsh -s /usr/bin/fish
+xdg-user-dirs-update
 sudo reboot
